6b4e8fc82c4df250c3f6344c7a6d2c20bc8b63e0
ruccho <noboroo2000@yahoo.co.jp>
2024-10-12 19:34:17 +0900

    wit2waas

---
+++ WaaS/wit2waas/src/source.rs
@@ -0,0 +1,27 @@
+﻿#[derive(Default)]
+pub struct Source {
+    content: String,
+    indent: u32
+}
+
+impl Source {
+    pub fn push_line(&mut self, line: &str) {
+        self.content.push_str(&"    ".repeat(self.indent as usize));
+        self.content.push_str(line);
+        self.content.push_str("\n");
+    }
+
+    pub fn open_block(&mut self) {
+        self.push_line("{");
+        self.indent += 1;
+    }
+
+    pub fn close_block(&mut self) {
+        self.indent -= 1;
+        self.push_line("}");
+    }
+    
+    pub fn finish(self) -> String {
+        self.content
+    }
+}
